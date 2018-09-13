035b58ae602f27ac3a4b1e7340f3796330875b5d
Thomas Adjadj <thomas.adjadj@outlook.com>
2018-09-13 12:40:02 +0200

    Update PopupWindow.js

--- src/PopupWindow.js
+++ src/PopupWindow.js
@@ -48,7 +48,7 @@ export class PopupWindow {
                 window["popupCallback_" + params.id] = this._callback.bind(this);
             }
 
-            this._popup.focus();
+           
             this._popup.window.location = params.url;
         }
 
